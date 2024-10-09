8bdefe04bcdf8a97759b422986a6cfc063b02cd7
Dylan-bak <jesuscarecrow@gmail.com>
2024-10-10 00:01:10 +0900

    Feat: [puppeteer] puppeteer를 이용하여 id/pw 입력 후 encryptedPassword 인터셉트

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,8 +1,40 @@
 import { NextResponse } from "next/server";
+import puppeteer, { type HTTPRequest } from "puppeteer";
 
 export async function POST(req: Request) {
   try {
-    return NextResponse.json("here", { status: 200 });
+    const { id: loginKey, pw: password } = await req.json();
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://app.catchtable.co.kr/ct/loginPhone");
+    await page.waitForSelector("#login-id");
+    await page.type("#login-id", loginKey);
+    await page.waitForSelector("#login-pw");
+    await page.type("#login-pw", password);
+
+    await page.setRequestInterception(true);
+    const requestPromise = new Promise<string>((resolve) => {
+      page.on("request", (interceptedRequest: HTTPRequest) => {
+        if (interceptedRequest.url().includes("/login-via-catchtable")) {
+          const postData = interceptedRequest.postData();
+
+          if (postData) {
+            const jsonData = JSON.parse(postData);
+            const encryptedPassword = jsonData.encryptedPassword || "";
+            console.log("Encrypted Password1:", encryptedPassword);
+            resolve(encryptedPassword);
+          }
+        }
+        interceptedRequest.continue();
+      });
+    });
+
+    await page.click('#main #login-form button[type="submit"]');
+    await page.waitForNavigation();
+    const encryptedPassword = await requestPromise;
+    await browser.close();
+    return NextResponse.json({ loginKey, encryptedPassword }, { status: 200 });
   } catch (error) {
     console.error("Error processing image:", error);
     return NextResponse.json(
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import axios from "axios";
+import { useState } from "react";
 
 export default function () {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
   const handleReserve = async () => {
     try {
-      const response = await axios.post("/api/login", {
-        name: "John Doe",
-        date: "2023-10-01",
-        time: "18:00",
-      });
+      const response = await axios.post("/api/login", { id, pw });
       console.log("Reservation successful:", response.data);
     } catch (error) {
       console.error("Error making reservation:", error);
@@ -20,6 +20,16 @@ export default function () {
     <div className="h-full w-full px-8 pb-8">
       <div className="flex items-start justify-between px-6 sm:flex-row sm:items-center">
         <div className={`w-full`}>
+          <input
+            className={`border`}
+            placeholder="ID"
+            onChange={(e) => setId(e.target.value)}
+          />
+          <input
+            className={`border`}
+            placeholder="PW"
+            onChange={(e) => setPw(e.target.value)}
+          />
           <button
             onClick={handleReserve}
             className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
