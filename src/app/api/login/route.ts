575e8b39f5a0f705ea0e2c0034d0557c551a057c
Dylan-bak <jesuscarecrow@gmail.com>
2024-12-04 13:58:30 +0900

    Feat: [login] ing

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -15,35 +15,71 @@ export async function POST(req: Request) {
     await page.waitForSelector("#login-pw");
     await page.type("#login-pw", password);
 
-    await page.setRequestInterception(true);
-    const requestPromise = new Promise<string>((resolve) => {
-      page.on("request", (interceptedRequest: HTTPRequest) => {
-        if (interceptedRequest.url() === loginApi) {
-          const postData = interceptedRequest.postData();
+    // await page.setRequestInterception(true);
+    // const requestPromise = new Promise<string>((resolve) => {
+    //   page.on("request", (interceptedRequest: HTTPRequest) => {
+    //     if (interceptedRequest.url() === loginApi) {
+    //       const postData = interceptedRequest.postData();
 
-          if (postData) {
-            const jsonData = JSON.parse(postData);
-            const encryptedPassword = jsonData.encryptedPassword || "";
-            console.log("Encrypted Password1:", encryptedPassword);
-            resolve(encryptedPassword);
-          }
-        }
-        interceptedRequest.continue();
-      });
-    });
+    //       if (postData) {
+    //         const jsonData = JSON.parse(postData);
+    //         const encryptedPassword = jsonData.encryptedPassword || "";
+    //         resolve(encryptedPassword);
+    //       }
+    //     }
+    //     interceptedRequest.continue();
+    //   });
+    // });
 
     await page.click('#main #login-form button[type="submit"]');
     await page.waitForNavigation();
-    const encryptedPassword = await requestPromise;
-    await browser.close();
+    // const encryptedPassword = await requestPromise;
 
-    const response = await axios.post(
-      loginApi,
-      { loginKey, encryptedPassword },
-      { headers }
+    /** 여기서부터 puppeteer를 이용한 시도 */
+    await page.goto("https://app.catchtable.co.kr/ct/shop/somit_park");
+    // await page.goto("https://app.catchtable.co.kr/ct/shop/viatoledo");
+    await page.waitForSelector("#dock button[type='button']");
+    await page.evaluate(() => {
+      const button = document.querySelector(
+        "#dock button[type='button']"
+      ) as HTMLButtonElement;
+      button.click();
+    });
+
+    const date = "금요일, 10월 11, 2024";
+    debugger;
+    // await page.waitForSelector(
+    //   `.mbsc-calendar-slide-active [aria-label=${date}]`
+    // );
+    await page.waitForSelector(
+      `.mbsc-calendar-slide-active [aria-label="금요일, 10월 11, 2024"]`
     );
+    await page.click(`.mbsc-calendar-slide-active [aria-label=${date}]`);
 
-    return NextResponse.json({ response }, { status: 200 });
+    await page.waitForSelector(".drawer-box .timeslot-swiper span");
+    const timeText = "오후 7:00";
+    await page.evaluate((timeText) => {
+      debugger;
+      const targetElement = Array.from(
+        document.querySelectorAll(".drawer-box .timeslot-swiper span")
+      ).find((element) => {
+        window.getComputedStyle(element).display !== "none" &&
+          element.textContent?.trim() === timeText;
+      });
+
+      if (targetElement) targetElement.closest("button")?.click();
+    }, timeText);
+    /** 여기서부터 puppeteer를 이용한 시도 끝 */
+
+    //  await browser.close();
+    // const response = await axios.post(
+    //   loginApi,
+    //   { loginKey, encryptedPassword },
+    //   { headers }
+    // );
+
+    return NextResponse.json({ message: "here" }, { status: 200 });
+    // return NextResponse.json({ ...response.data }, { status: 200 });
   } catch (error) {
     console.error("Error processing image:", error);
     return NextResponse.json(
